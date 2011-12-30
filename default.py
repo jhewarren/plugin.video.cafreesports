a8540ef734fa96afd8b8e9e7a8acabedad50a68b
Micah Galizia <micahgalizia@gmail.com>
2011-12-30 00:27:26 -0500

    handle those fucking smil files

--- default.py
+++ default.py
@@ -15,7 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
 
-import xbmcplugin, xbmcgui, xbmcaddon
+import xbmcplugin, xbmcgui, xbmcaddon, urllib
 import tsn
 
 
@@ -51,26 +51,67 @@ def createTSNMenu():
         return
     
     for stream_page in stream_pages:
+        
+        item_url = None
+        is_folder = False
+        
         rtmp_stream = my_tsn.getRTMPStream(stream_page[1])
-        if rtmp_stream == None:
-            print "No RTMP stream for '" + stream_page[1]
-            continue
+        if rtmp_stream != None:
+            item_url = rtmp_stream + " live=true"
+        else:
+            http_smil = my_tsn.getHTTPSMIL(stream_page[1])
+            item_url = sys.argv[0] + "?id=smil_tsn&smil=" + urllib.quote_plus(http_smil)
+            is_folder = True
 
         title = stream_page[0].title()
         
         li = xbmcgui.ListItem(title)
         li.setInfo( type="Video", infoLabels={"Title" : title})
         xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),
-                                    url=rtmp_stream + " live=true",
+                                    url=item_url,
                                     listitem=li,
-                                    isFolder=False)
+                                    isFolder=is_folder)
         
     # Finish listing streams
     xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))
+    return
 
 
+def createTSNSMILMenu(smil_uri):
+    """
+    Load a TSN smil file and add the stream options to the list
+    """
+    print "SMIL URI IS '" + smil_uri + "'"
+    
+    my_tsn = tsn.TSN()
+    streams = my_tsn.getHTTPStreams(smil_uri)
+    
+    if streams == None:
+        print "Unable to parse SMIL files."
+        xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))
+        return
+        
+    for stream in streams:
+        title = stream[0]
+        item_url = stream[1]
+        
+        li = xbmcgui.ListItem(title)
+        li.setInfo( type="Video", infoLabels={"Title" : title})
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),
+                                    url=item_url,
+                                    listitem=li,
+                                    isFolder=False)
+
+        
+    xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))
+    return
+
+#?id=smil_tsn&smil=http%3A%2F%2Fmanifest.ctv.ca.edgesuite.net%2FManifest%2F2011%2F12%2F29%2FDenVrsCanGeo2.smil
 # if the second argument is empty this is the main menu
+
 if (len(sys.argv[2]) == 0):
     createMainMenu()
 elif sys.argv[2] == '?id=tsn':
     createTSNMenu()
+elif sys.argv[2][:12] == "?id=smil_tsn":
+    createTSNSMILMenu(urllib.unquote(sys.argv[2][18:len(sys.argv[2])]))
--- tsn.py
+++ tsn.py
@@ -1,8 +1,11 @@
-import urllib, urllib2, time, random, re
+import urllib, urllib2, time, random, re, xml.dom.minidom
 
 class TSN:
     """
     Class to scrape TSN for a live stream of sports. Usually hockey
+    
+    @todo getVideoURL could check the JSON err variable return for further
+          validation
     """
 
     def __init__(self):
@@ -13,9 +16,13 @@ class TSN:
         self.MAIN_PAGE = 'http://tsn.ca'
         self.LIVE_LINK = 'tsn.ca/live'
         self.BANNER_PREFIX = 'matchup-banner">'
-        self.CLIP_ID_PREFIX = 'clipid:'
+        self.RTMP_ID_PREFIX = 'clipid:'
+        self.HTTP_ID_PREFIX = 'mpflashvars.id = '
+        self.ELEMENT_ID_PREFIX = '<element id="'
         self.URL_PREFIX = 'url:'
         self.BASE_RTMP_LOOKUP = 'http://cls.ctvdigital.net/cliplookup.aspx?'
+        self.BASE_ELMNT_LOOKUP = 'http://esi.ctv.ca/datafeed/content_much.aspx?'
+        self.BASE_HTTP_LOOKUP = 'http://esi.ctv.ca/datafeed/flv/urlgenjs.aspx?'
 
 
     def getLiveStreamPage(self):
@@ -86,7 +93,6 @@ class TSN:
     def getTitleInDiv(self, link, div_html):
         
         title = link
-        print "\n" + div_html + "\n"
 
         # see if there is an H2 to use
         match = re.search("<h2>(.*?)</h2>", div_html)
@@ -154,22 +160,23 @@ class TSN:
         """
         
         # get the clip ID and ensure it is valid
-        clip_id = self.getClipdID(stream_page)
+        clip_id = self.getID(stream_page, self.RTMP_ID_PREFIX, ',')
         if clip_id == None:
             return None
 
-        print "Clip ID is '" + clip_id + "'"
-        
-        return self.getRTMPURL(stream_page, clip_id)
+        return self.getVideoURL(self, self.BASE_RTMP_LOOKUP, "id", stream_page,
+                                clip_id)
 
-    def getClipdID(self, stream_page):
+    
+    def getID(self, stream_page, prefix, delimiter):
         """
         Get the clip id from the page that contains the stream player.
         
         @param stream_page The page that contains the stream player. This page
                            will be parsed and the RTMP stream will be extracted.
+
+        @param prefix The value prefixing the 
         """
-        
         # get the main page
         req = urllib2.Request(stream_page)
 
@@ -184,22 +191,22 @@ class TSN:
         page_html = resp.read().lower()
 
         # find the start of the clip id
-        start_idx = page_html.find(self.CLIP_ID_PREFIX)
+        start_idx = page_html.find(prefix)
         if start_idx == -1:
             return None
         
         # find the end index of the 
-        end_idx = page_html[start_idx:len(page_html)].find(',')
+        end_idx = page_html[start_idx:len(page_html)].find(delimiter)
         if end_idx == -1:
             return None
         end_idx = end_idx + start_idx        
         
-        return page_html[start_idx + len(self.CLIP_ID_PREFIX):end_idx]
-
-
-    def getRTMPURL(self, stream_page, clip_id):
+        return page_html[start_idx + len(prefix):end_idx]
+        
+        
+    def getVideoURL(self, base, id_var, stream_page, clip_id):
         """
-        Get the RTMP URL. The URL is returned by a request like:
+        Get the video URL. The URL is returned by a request like:
         
         http://cls.ctvdigital.net/cliplookup.aspx?id=547533&timeZone=-5&random=8627676
 
@@ -214,12 +221,10 @@ class TSN:
         tz_str = str(-1 * time.timezone / 3600)        
 
         # generate the request URL        
-        req_url = self.BASE_RTMP_LOOKUP
-        req_url = req_url + "id="+ urllib.quote_plus(clip_id)
+        req_url = base
+        req_url = req_url + id_var + "="+ urllib.quote_plus(clip_id)
         req_url = req_url + "&timezone=" + urllib.quote_plus(tz_str)
         req_url = req_url + "&random=" + urllib.quote_plus(rand_str)
-        
-        print req_url
         
         # get the main page
         req = urllib2.Request(req_url)
@@ -234,8 +239,6 @@ class TSN:
 
         page_html = resp.read()
         
-        print page_html
-        
         # find the start index (add one for the ') 
         start_idx = page_html.find(self.URL_PREFIX)
         if start_idx == -1:
@@ -249,3 +252,117 @@ class TSN:
         end_idx = end_idx + start_idx
         
         return page_html[start_idx:end_idx]
+    
+    
+    def getHTTPSMIL(self, stream_page):
+        """
+        Get the HTTP Streams
+        """
+        
+        # Get the HTTP ID
+        http_id = self.getID(stream_page, self.HTTP_ID_PREFIX, ';')
+        if http_id == None:
+            return None
+        
+        # Get the Element ID
+        element_id = self.getHTTPElement(http_id)
+        if element_id == None:
+            return None
+        
+        return self.getVideoURL(self.BASE_HTTP_LOOKUP, "vid", stream_page, element_id)
+
+    def getHTTPElement(self, http_id):
+        """
+        Get the element value for later use in getting the SMIL file
+        """
+
+        # create the request
+        req_url = self.BASE_ELMNT_LOOKUP + "cid="+ urllib.quote_plus(http_id)
+        req = urllib2.Request(req_url)
+        
+        # make the request
+        try:
+            resp = urllib2.urlopen(req)
+        except:
+            print "URL error trying to open page"
+            return None
+
+        page_html = resp.read()
+        
+        # find the start index (add one for the ') 
+        start_idx = page_html.find(self.ELEMENT_ID_PREFIX)
+        if start_idx == -1:
+            return None
+        start_idx = start_idx + len(self.ELEMENT_ID_PREFIX)
+        
+        # find the end index
+        end_idx = page_html[start_idx:len(page_html)].find('"')
+        if end_idx == -1:
+            return None
+        end_idx = end_idx + start_idx
+        
+        return page_html[start_idx:end_idx]
+
+
+    def getHTTPStreams(self, smil_uri):
+        """
+        Get the smil, parse it up and return a list of lists containing names
+        and streams.
+        """
+        req = urllib2.Request(smil_uri)
+        
+        # make the request
+        try:
+            resp = urllib2.urlopen(req)
+        except:
+            print "URL error trying to open page"
+            return None
+
+        dom = xml.dom.minidom.parseString(resp.read())
+        head_node = dom.getElementsByTagName('head')[0]
+        base_uri = None
+
+        # loop over the meta tags and find the base video URI
+        for meta in head_node.getElementsByTagName('meta'):
+            
+            # make sure we are dealing with httpBase
+            if meta.hasAttribute('name'):
+                if meta.getAttributeNode('name').value != 'httpBase':
+                    continue
+                
+                # get the base stream URL
+                if meta.hasAttribute('content'):
+                    base_uri = meta.getAttributeNode('content').value
+                    break
+        
+        # at this point we should have the base
+        if base_uri == None:
+            print "ERROR: unable to parse base URI."
+            return None
+
+        print "base uri is '" + base_uri + "'"
+
+        streams = []
+        body_node = dom.getElementsByTagName('body')[0]
+        for video in body_node.getElementsByTagName('video'):
+            
+            # make sure there is a source
+            if not video.hasAttribute('src'):
+                continue
+
+            # set the source and default the title value            
+            source = video.getAttributeNode('src').value
+            title = source
+            
+            # I dont know why this is needed, but it is
+            source = base_uri + source + "?v=1&fp=1&r=1&g=1"
+
+            # get an actual title
+            if video.hasAttribute('system-bitrate'):
+                title = video.getAttributeNode('system-bitrate').value
+                title = title + " bps"
+
+            # add the stream
+            streams.append([title, source])
+
+        return streams
